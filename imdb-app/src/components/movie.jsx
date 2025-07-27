aac877acf76cbe457c9bd195fb705cb8b10d9e5c
rrashmi642 <0173cs191049@gmail.com>
2025-07-27 21:01:12 +0530

    Watchlist button aligned

--- imdb-app/src/components/movie.jsx
+++ imdb-app/src/components/movie.jsx
@@ -1,23 +1,30 @@
 // import MovieList from "./movieList";
 // import MovieListPage from "../pages/movieListPage";
-import '../pages/App.css'
-import App from '../App';
+import "../pages/App.css";
+import App from "../App";
 
-const Movie = ({movie, watchlist, setWatchList})=>{
-    const clickbtnhandler=()=>{
-        console.log(movie.id, movie);
-        setWatchList({...watchlist, [movie.id]:movie});
-        console.log("watchlist", watchlist);
-    }
-    return (
-        <div className="movie">
-        <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}alt={movie.title}/>
+const Movie = ({ movie, watchlist, setWatchList }) => {
+  const clickbtnhandler = () => {
+    console.log(movie.id, movie);
+    setWatchList({ ...watchlist, [movie.id]: movie });
+    console.log("watchlist", watchlist);
+  };
+  return (
+    <div className="movie">
+      
+        <img
+          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          alt={movie.title}
+        />
         <div className="movie-info">
-            <h3>{movie.title}</h3>
-            <button className="watchlist" onClick={clickbtnhandler}>{watchlist[movie.id]?'-': '+'}Add Watchlist</button>
-
+          <h3>{movie.title}</h3>
+        
+        <button className="watchlistbutton" onClick={clickbtnhandler}>
+          {watchlist[movie.id] ? "-" : "+"}Add Watchlist
+        </button>
         </div>
-
-    </div>)
-}
+    </div>
+   
+  );
+};
 export default Movie;
--- imdb-app/src/pages/header.jsx
+++ imdb-app/src/pages/header.jsx
@@ -3,9 +3,9 @@ import {Link} from 'react-router'
 const Header= () =>{
     return (
         <div className="header">
-            <h1 className="logo" marginTop= '10px'> IMDB</h1>
+            <h1 className="logo" marginTop= '10px'> Movio</h1>
             <ul className="nav">
-                <li ><Link to="/" color="yellow">MovieList </Link></li> 
+                <li ><Link className= "" to="/" color="yellow">MovieList </Link></li> 
                <li><Link to="/watchlist">My Watchlist</Link></li> 
                {/* <li><Link>MovieDetails</Link> </li>                 */}
             </ul>
