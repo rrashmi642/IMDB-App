bd278c9d660bc7387c2304730e4f30dfa51ea471
rrashmi642 <0173cs191049@gmail.com>
2025-07-27 21:05:48 +0530

    jsx added

--- imdb-app/src/App.jsx
+++ imdb-app/src/App.jsx
@@ -3,9 +3,9 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 // import "./App.css";
 import Header from "./pages/header";
-import MovieListPage from "./pages/movieListPage";
-import WatchListPage from "./pages/watchListPage";
-import MovieDetailsPage from "./pages/MovieDetailsPage";
+import MovieListPage from "./pages/movieListPage.jsx";
+import WatchListPage from "./pages/watchListPage.jsx";
+import MovieDetailsPage from "./pages/MovieDetailsPage.jsx";
 
 import { BrowserRouter, Route, Routes } from "react-router";
 function App() {
